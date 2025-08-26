b11c4e4b898ef7ad3066e66d7c5e3036ef1fdaa6
Nils Harbke <nils.harbke@proton.me>
2025-08-26 12:34:50 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs @ { flake-parts, ... }:
+  outputs = inputs @ { nixpkgs, flake-parts, ... }:
   flake-parts.lib.mkFlake { inherit inputs; } {
 
     systems = [ "x86_64-linux" "aarch64-linux" ];
